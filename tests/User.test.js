64659499e349dde864440b50f0e5d9cd5a066bc5
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-19 04:30:06 +0100

    Added some 'Instances' test cases to user test file

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,55 +1,23 @@
-const User = require("../main/User");
+const { User } = require("../main/User");
 
-describe("Creating User instances and validating it", function() {
-  expect(new User("Martins", "martins@gmail.com", "pass1234")).toMatch(
-    /created succesfully/
-  );
+jest.mock("../fs");
 
-  expect(new User("Martins", "martins@gmail.com", "pass1234")).toMatch(
-    /Username already exist/
-  );
+describe("Testing User Instances and saving it to Database", function() {
+  it("creating new User instance", function() {
+    let martins = new User("Martins", "test@gmail.com", "pass1234");
 
-  expect(new User(3, "martins@gmail.com", "pass1234")).toMatch(
-    /Must be String/
-  );
+    expect(martins).toBeDefined();
+  });
+  it("Saving instances to Database", function() {
+    let martins = new User("Martins", "test@gmail.com", "pass1234");
 
-  expect(new User("martins@gmail.com", "pass1234")).toThrowError();
+    expect(martins.save()).toMatchObject(martins);
+  });
+
+  it("New User instances with JS 'instanceOf' ", function() {
+    let martins = new User("Martins", "test@gmail.com", "pass1234");
+
+    expect(martins instanceof User).toBeTruthy();
+  });
 });
 
-
-describe("Find user by their Id", function() {
-
-  let noName = User.findById();
-  let victor = User.findById(2);
-  let notFound = User.findById(2000);
-  let missing = User.findById("noOne");
-
-  expect(noName).toMatch(/Input must be a valid userId/)
-  expect(victor).toBeDefined()
-  expect(notFound).toBeUndefined()
-  expect(missing).toMatch(/Input must be a valid userId/)
-})
-
-
-describe("Update user details", function() {
-  let martins = new User("Martins", "martins@gmail.com", "pass1234");
-  let victor = new User("Victor", "martins@gmail.com", "pass1234");
-
-  expect(
-    martins.updateDetails("NewMartins", "martins@gmail.com", "pass1234")
-  ).toReturn({});
-  expect(victor.updateDetails("NewMartins", "martins@gmail.com", "pass1234")).toMatch(/User with this name already exist/)
-});
-
-describe("Find user by their username", function() {
-
-  let noName = User.findById("noOne");
-  let victor = User.findById("Victor");
-  let notFound = User.findById(2000);
-  let missing = User.findById("noOne");  
-
-  expect(noName).toMatch(/Input must be a valid username/)
-  expect(victor).toBeDefined()
-  expect(notFound).toBeUndefined()
-  expect(missing).toMatch(/Input must be a valid userId/)
-})
