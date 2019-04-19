2073ec55e0dbf529f7d22b341a066c2434c7dd09
iKnowJavaScript <remymartins0398@yahoo.com>
2019-04-19 08:08:13 +0100

    Add test cases for updating Admin details and saving it to the database

--- tests/Admin.test.js
+++ tests/Admin.test.js
@@ -1,6 +1,6 @@
-const {Admin} = require("../main/Admin")
+const { Admin } = require("../main/Admin");
 
-jest.mock("../fs"); 
+jest.mock("../fs");
 
 describe("Testing Admin Instances and saving it to Database", function() {
   it("Creating new Admin instance", function() {
@@ -11,7 +11,7 @@ describe("Testing Admin Instances and saving it to Database", function() {
   it("Checking if isAdmin is true", function() {
     let admin = new Admin("Martins", "test@gmail.com", "pass1234");
 
-    expect(admin.isAdmin).toBeTruthy()
+    expect(admin.isAdmin).toBeTruthy();
   });
   it("Saving instances to Database", function() {
     let admin = new Admin("Martins", "test@gmail.com", "pass1234");
@@ -23,5 +23,23 @@ describe("Testing Admin Instances and saving it to Database", function() {
     let admin = new Admin("Martins", "test@gmail.com", "pass1234");
 
     expect(admin instanceof Admin).toBeTruthy();
+  });
+});
+
+describe("Updating Admin details", function() {
+  let admin;
+  beforeEach(function() {
+    admin = new Admin("Martins", "martins@gmail.com", "pass1234");
+    admin.save();
+  });
+  it("Validating Admin input before updating the Databae", function() {
+    expect(admin.updateDetail(4444, "martins@gmail.com", "newPass")).toMatch(
+      /Input a valid details/
+    );
+  });
+  it("Updating Admin details on Valid inputs", function() {
+    expect(admin.updateDetail("New", "martins@gmail.com", "newPass")).toMatch(
+      /Updated Succesfully/
+    );
   });
 });
